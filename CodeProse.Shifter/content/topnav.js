ccb1f91e3f6a916a0f849c5c62ba54b2e677daff
Samuel Nissim <samnissim@gmail.com>
2012-02-06 12:15:39 -0600

    - moved topnav to JS script - new separate wireframe for view-shift - added actions to view-shift - UI changes

---
+++ CodeProse.Shifter/content/topnav.js
@@ -0,0 +1,9 @@
+﻿var user; // global
+
+$(function () {
+    user = { name: "Raul Aso" }; // get from cookie
+
+    $('<div id="topnav"><a href="default.htm">Home</a> <a href="view-calendar.htm">View</a> <a href="manage.htm">Manage</a></div>')
+        .append($('<div id="user" />').text(user.name))
+        .prependTo("body");
+});
