b1e06570555984e2487bd72131682150d0e10908
Samuel Nissim <samnissim@gmail.com>
2012-02-08 10:18:42 -0600

    deletebutton jquery plugin applied theme sitewide fixed various links

---
+++ CodeProse.Shifter/content/jquery.deletebutton.js
@@ -0,0 +1,42 @@
+﻿(function ($) {
+
+    $.fn.deletebutton = function (options) {
+
+        var settings = $.extend({
+            icon: "ui-icon-closethick",
+            confirm: "Are you sure you want to delete this item?",
+            label: "Delete",
+            width: "22px",
+            height: "22px",
+            click: function () { }
+        }, options);
+
+        return this.each(function () {
+
+            var $this = $(this);
+
+            $this.append($("<button></button>")
+                    .text(settings.label)
+                    .button({
+                        icons: { primary: settings.icon },
+                        text: false
+                    }).css({
+                        "float": "right",
+                        "width": settings.width,
+                        "height": settings.height
+                    }).click(function () {
+                        if (confirm(settings.confirm)) {
+                            settings.click();
+
+                            $(this).parent().remove();
+                        }
+                    }).hide())
+                .hover(function () {
+                    $(this).children("button").show();
+                },
+                function () {
+                    $(this).children("button").hide();
+                });
+        });
+    };
+})(jQuery);
