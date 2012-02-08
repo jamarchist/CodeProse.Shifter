1cba5c47cb384a30550d96bcf30a63b003df86aa
Samuel Nissim <samnissim@gmail.com>
2012-02-08 11:04:39 -0600

    CSS

--- CodeProse.Shifter/content/topnav.js
+++ CodeProse.Shifter/content/topnav.js
@@ -3,7 +3,13 @@
 $(function () {
     user = { name: "Raul Aso" }; // get from cookie
 
-    $('<div id="topnav" class="ui-widget-header"><a href="default">Home</a> <a href="view-calendar">View</a> <a href="assign-shifts">Manage</a></div>')
+    $('<div id="topnav" class="ui-widget-header">' +
+            '<a href="default">Home</a> ' +
+            '<a href="view-calendar">Calendar</a> ' +
+            '<a href="manage-shifts">Shifts</a> ' +
+            '<a href="manage-members">Members</a> ' +
+            '<a href="assign-shifts">Schedule</a>' +
+            '</div>')
         .append($('<div id="user" />').text(user.name))
         .prependTo("body");
 });
