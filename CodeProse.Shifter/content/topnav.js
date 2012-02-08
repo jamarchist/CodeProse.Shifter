9af7d98451d06cd14629884332d44f125197224e
Samuel Nissim <samnissim@gmail.com>
2012-02-07 23:37:36 -0600

    began incorporating jquery UI "Sunny" theme into wireframes

--- CodeProse.Shifter/content/topnav.js
+++ CodeProse.Shifter/content/topnav.js
@@ -3,7 +3,7 @@
 $(function () {
     user = { name: "Raul Aso" }; // get from cookie
 
-    $('<div id="topnav"><a href="default.htm">Home</a> <a href="view-calendar.htm">View</a> <a href="manage.htm">Manage</a></div>')
+    $('<div id="topnav" class="ui-widget-header"><a href="default.htm">Home</a> <a href="view-calendar.htm">View</a> <a href="manage.htm">Manage</a></div>')
         .append($('<div id="user" />').text(user.name))
         .prependTo("body");
 });
