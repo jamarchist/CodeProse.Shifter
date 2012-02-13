9b2ab222c8fd9790d534d69293f06c65f7d9807b
Ryan@Ryan-HP <Ryan@Ryan-HP>
2012-02-13 00:32:35 -0600

    Added testing project. Just sticking with xunit since that's what Nancy.Testing comes with.

---
+++ CodeProse.Shifter.Tests/LoginTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeProse.Shifter.authentication;
+using Xunit;
+using Nancy.Testing;
+
+namespace CodeProse.Shifter.Tests
+{
+    public class LoginTests
+    {
+        [Fact]
+        public void CanGetLoginPage()
+        {
+            var browser = new Browser(new BootStrapper());
+            var response = browser.Get("/login", with => with.HttpRequest());
+
+            response.Body["form"].ShouldExist();
+        }
+    }
+}
