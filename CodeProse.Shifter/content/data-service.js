6615afa588cbe76f1dc616c26381dcf1f06f4e64
Ryan@Ryan-HP <Ryan@Ryan-HP>
2012-04-06 19:08:42 -0500

    Just pushing these changes so that everything is in source control. We really need to figure out how to make dates easy.

--- CodeProse.Shifter/content/data-service.js
+++ CodeProse.Shifter/content/data-service.js
@@ -1,12 +1,12 @@
 ﻿var CodeProse = CodeProse || {};
 CodeProse.Shifter = CodeProse.Shifter || {};
-CodeProse.Shifter.Data = CodeProse.Shifter.Data || {};
+CodeProse.Shifter.Resources = CodeProse.Shifter.Resources || {};
 
-CodeProse.Shifter.Data.addMember = function(newMember, onSuccess) {
+CodeProse.Shifter.Resources.add = function(resourceUrl, newResource, onSuccess) {
     $.ajax({
         type: "POST",
-        url: "/shifter/members/",
-        data: JSON.stringify(newMember),
+        url: "/shifter" + resourceUrl,
+        data: JSON.stringify(newResource),
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: onSuccess,
@@ -16,24 +16,10 @@ CodeProse.Shifter.Data.addMember = function(newMember, onSuccess) {
     });
 };
 
-CodeProse.Shifter.Data.getMembers = function(onSuccess) {
-    $.ajax({
-        type: "GET",
-        url: "/shifter/members/",
-        data: "",
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: onSuccess,
-        error: function(msg) {
-            console.log(msg);
-        }
-    });
-};
-
-CodeProse.Shifter.Data.deleteMember = function(identifier, onSuccess) {
+CodeProse.Shifter.Resources.del = function(resourceUrl, onSuccess) {
     $.ajax({
         type: "DELETE",
-        url: "/shifter/members/" + identifier,
+        url: "/shifter" + resourceUrl,
         data: "",
         contentType: "application/json; charset=utf-8",
         success: onSuccess,
@@ -43,7 +29,7 @@ CodeProse.Shifter.Data.deleteMember = function(identifier, onSuccess) {
     });
 };
 
-CodeProse.Shifter.Data.get = function (resourceUrl, onSuccess) {
+CodeProse.Shifter.Resources.get = function (resourceUrl, onSuccess) {
     $.ajax({
         type: "GET",
         url: '/shifter' + resourceUrl,
---
+++ CodeProse.Shifter/content/shifter-date.js
@@ -0,0 +1,6 @@
+﻿var CodeProse = CodeProse || {};
+CodeProse.Shifter = CodeProse.Shifter || {};
+
+CodeProse.Shifter.ShifterDate = function(year, month, day, hour, minute) {
+    var jsDate = new Date(year, month - 1, hour, minute, 0);
+};
